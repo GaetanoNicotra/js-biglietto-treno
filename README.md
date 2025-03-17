5555373613af8976b5ce2e01eb7928593693fba4
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-17 17:40:26 +0100

    [DONE] Sistemati i commenti nel file js e sistemati i passaggi da effettuare nel file README.md.  Esercizio terminato

--- README.md
+++ README.md
@@ -13,17 +13,28 @@ Ricordatevi di spezzare il problema in sottoproblemi come visto a lezione nel fi
 
 // Calcolare il prezzo di un bigletto del treno sulla base dei chilometri da percorrere e l'età del passeggero
 
-// Chiedere all'utente il numero di chilometri da percorrere e la sua età:
+// Chiedere all'utente il numero di chilometri da percorrere
+// Chiedere all'utente la sua età
 
 // definiamo una variabile che contiene il numero dei Km da percorrere
 // definiamo una variabile che contine l'età del passeggero
+// calcoliamo il prezzo dei biglietto in base alla tariffa (0.21€ al Km )
+// definiamo un a variabile per il calcolo
+// mostriamo il risultato in console
 
+// Calcoliamo il prezzo del biglietto in base all'età:
 
-// Calcoliamo il prezzo del biglietto:
+// se il paseggero è minorenne
+// definiamo una variabile per il calcolo
+// applichiamo il 20% di sconto al prezzo iniziale del biglietto
+// mostriamo il risultato in console
 
-// definiamo una variabile per il calcolo 
+// se il paseggero è over 65
+// definiamo una variabile per il calcolo
+// applichiamo il 40% di sconto al prezzo iniziale del biglietto
+// mostriamo il risultato in console
 
-// moltiplichiamo il numero dei chilometeri da percorre per euro 0.21
-// se il passeggero è minorenne applichiamo uno sconto del 20%
-// se il passeggero è un over 65 applichiamo uno sconto del 40%
+// se l'età del passeggero è compresa tra i 18 e i 65 anni
+// mostriamo in console il prezzo del biglietto senza alcuno sconto
+
 // per finire calcoliamo il prezzo totale del biglietto con massimo due cifre decimali e lo mostriamo in console 
--- script.js
+++ script.js
@@ -6,8 +6,9 @@
 let distance = prompt("Inserisci il numero dei Km da percorrere");
 let age = prompt("Inserisci la tua età")
 
-// calcoliamo il prezzo dei chilometri in base alla tariffa (0.21€ al Km )
-// definiamo un a variabile per il calcolo
+// calcoliamo il prezzo del biglietto moltiplicando i chilometri da percorrere per la tariffa: (0.21€ al Km )
+
+// definiamo una variabile per il calcolo
 // mostriamo il risultato in console
 
 let distancePrice = (distance * 0.21).toFixed(2);
@@ -17,17 +18,25 @@ console.log(distancePrice + " " + "Euro (Prezzo intero)");
 // calcoliamo il prezzo dei chilometri in base all'età
 
 // se il passeggero è minorenne applichiamo uno sconto del 20% sul prezzo del biglietto
+// definiamo una variabile per il prezzo scontato del 20%
 // mostriamo il risultato in console
 
 if(age < 18){
-    let priceUnderage = (distancePrice * 20 / 100).toFixed(2);
+    let priceUnderage = (distancePrice - distancePrice * 20 / 100).toFixed(2);
     console.log(priceUnderage  + " " + "Euro (prezzo scontato per minorenni)");
 }
 
+// se il passeggero è over 65 applichiamo uno sconto del 40% sul prezzo del biglietto
+// definiamo una variabile per il prezzo scontato del 40%
+// mostriamo il risultato in console
+
 else if(age > 65){
-    let priceOver65 = (distancePrice * 40 / 100).toFixed(2);
-    console.log(priceOver65  + " " + "Euro (Prezzo scontato per magiorenni)");
+    let priceOver65 = (distancePrice - distancePrice * 40 / 100).toFixed(2);
+    console.log(priceOver65  + " " + "Euro (Prezzo scontato per maggiorenni)");
 }
+
+//se il passeggero ha un'età compresa tra i 18 e i 65 anni il prezzo rimane invariato
+//mostriamo in console il prezzo iniziale
 
 else{
     console.log(distancePrice + " " + "Euro (Prezzo intero, non rientri in una fascia di età in sconto)")
