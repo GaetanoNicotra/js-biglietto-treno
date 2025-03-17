e52b48ac6387ab70e5ff26dfed88c61812c7ef10
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-17 17:03:22 +0100

    [UPD] scritte tutte le condizioni che possono verificarsi per applicare lo sconto

--- script.js
+++ script.js
@@ -3,12 +3,32 @@
 // definiamo una variabile che contiene il numero dei Km da percorrere
 // definiamo una variabile che contine l'età del passeggero
 
-let distance = prompt("Inserisci il numero dei KM da percorrere");
+let distance = prompt("Inserisci il numero dei Km da percorrere");
 let age = prompt("Inserisci la tua età")
 
-// calcoliamo il prezzo dei chilometri in basse alla tariffa (0.21€ al Km )
+// calcoliamo il prezzo dei chilometri in base alla tariffa (0.21€ al Km )
 // definiamo un a variabile per il calcolo
 // mostriamo il risultato in console
 
 let distancePrice = distance * 0.21;
-console.log(distancePrice);
+console.log(distancePrice + " " + "Euro (Prezzo intero)");
+
+
+// calcoliamo il prezzo dei chilometri in base all'età
+
+// se il passeggero è minorenne applichiamo uno sconto del 20% sul prezzo del biglietto
+// mostriamo il risultato in console
+
+if(age < 18){
+    let priceUnderage = distancePrice * 20 / 100;
+    console.log(priceUnderage  + " " + "Euro (prezzo scontato)");
+}
+
+else if(age > 65){
+    let priceOver65 = distancePrice * 40 / 100;
+    console.log(priceOver65  + " " + "Euro (Prezzo scontato)");
+}
+
+else{
+    console.log(distancePrice + " " + "Euro (Prezzo intero, non rientri in una fascia di età in sconto)")
+}
